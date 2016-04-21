de684a5999ef609974b08e3de66018b10e1f5820
stevenpantin <steven_pantin@baylor.edu>
2016-04-21 15:36:19 -0500

    Initial Commit

---
+++ GraphicsFinal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GraphicsFinal
+//
+//  Created by Steven Pantin on 4/21/16.
+//  Copyright © 2016 Steven Pantin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
